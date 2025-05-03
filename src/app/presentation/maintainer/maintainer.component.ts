7615ddc78a3bd01d1da8bfb276d7ee41282d37ab
Julius Böcker <julzbker@gmail.com>
2025-05-03 08:24:47 +0200

    Add login mock

--- src/app/presentation/maintainer/maintainer.component.ts
+++ src/app/presentation/maintainer/maintainer.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-maintainer',
-  imports: [TranslateModule],
+  imports: [
+    TranslateModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './maintainer.component.html',
   styleUrl: './maintainer.component.scss',
 })
-export class MaintainerComponent {}
+export class MaintainerComponent {
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+}
